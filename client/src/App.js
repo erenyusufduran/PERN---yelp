cc08c470e21be9d3f9a756a4e86ce93744a69e57
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-23 23:18:49 +0300

    context api

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { RestaurantsContextProvider } from "./context/Restourants.context";
 import Home from "./routes/Home";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 import UpdatePage from "./routes/UpdatePage";
 const App = () => {
   return (
-    <div className="container">
-      <Routes>
-        <Route index element={<Home />} />
-        <Route exact path="/restaurants/:id/update" element={<UpdatePage />} />
-        <Route
-          exact
-          path="/restaurants/:id"
-          element={<RestaurantDetailPage />}
-        />
-      </Routes>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route
+            exact
+            path="/restaurants/:id/update"
+            element={<UpdatePage />}
+          />
+          <Route
+            exact
+            path="/restaurants/:id"
+            element={<RestaurantDetailPage />}
+          />
+        </Routes>
+      </div>
+    </RestaurantsContextProvider>
   );
 };
 
