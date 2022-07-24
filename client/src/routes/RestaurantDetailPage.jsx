9738bcfa6bb7321ef34c46d7dbed10a706202a67
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-24 13:46:50 +0300

    review component layout

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -5,6 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { RestaurantsContext } from "../context/Restourants.context";
 import RestaurantFinder from "../apis/RestaurantFinder";
 import StarRating from "../components/StarRating";
+import Reviews from "../components/Reviews";
 
 const RestaurantDetailPage = () => {
   const { id } = useParams();
@@ -23,7 +24,17 @@ const RestaurantDetailPage = () => {
     fetchData();
   }, []);
 
-  return <div>{selectedRestaurant && <StarRating rating={2.8} />}</div>;
+  return (
+    <div>
+      {selectedRestaurant && (
+        <>
+          <div className="mt-3">
+            <Reviews />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default RestaurantDetailPage;
