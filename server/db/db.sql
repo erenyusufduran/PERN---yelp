b76eac4bc0531f06a38759ccfe350c8bb1b3672b
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-24 13:28:39 +0300

    pg tables

---
+++ server/db/db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE reviews (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check(rating >= 1 AND rating <= 5)
+);
+
+SELECT * FROM reviews WHERE restaurant_id = 25;
